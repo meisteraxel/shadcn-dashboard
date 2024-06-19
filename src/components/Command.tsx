93b3a46245c34b3120f9650c477ad3d59073cba6
Axel <meister.axel@gmail.com>
2024-06-19 11:55:21 +0200

    added notification menu to the header

--- src/components/Command.tsx
+++ src/components/Command.tsx
@@ -25,7 +25,7 @@ export function CommandDemo() {
   const [open, setOpen] = useState<boolean>(false);
 
   return (
-    <Command className="rounded-lg border shadow-md">
+    <Command className="rounded-lg border">
       <CommandInput placeholder="Type a command or search..." />
       {open && (
         <CommandList>
