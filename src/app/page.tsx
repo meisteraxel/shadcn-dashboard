0b3cbd317fe44c19fd3e8464179e64e4d19db713
Axel <meister.axel@gmail.com>
2024-06-19 15:24:11 +0200

    implemented data table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 import General from "@/components/Cards/General";
 import Calendar from "@/components/Cards/Calendar";
 import Lines from "@/components/Cards/Lines";
-import { Card } from "@/components/ui/card";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { TableDemo } from "@/components/Cards/Table";
 
 export default function Home() {
   return (
@@ -13,10 +19,16 @@ export default function Home() {
           <Calendar />
         </div>
       </div>
-      <div className="grid grid-cols-3 gap-8">
+      <div className="grid grid-cols-3 gap-8 h-[300px] overflow-hidden">
         <Lines />
-        <Card className="h-[300px]"></Card>
-        <Card className="h-[300px]"></Card>
+        <Card className="p-8 overflow-y-scroll">
+          <CardHeader>
+            <CardTitle>Orders</CardTitle>
+            <CardDescription>Orders of this month</CardDescription>
+          </CardHeader>
+          <TableDemo />
+        </Card>
+        <Card className=""></Card>
       </div>
     </div>
   );
