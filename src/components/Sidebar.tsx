0fa703d7a34990ab2955c0de38dc489c2ac6057b
Axel <meister.axel@gmail.com>
2024-06-19 10:13:06 +0200

    build basic structure of app

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import UserItem from "./UserItem";
+
+export default function Sidebar() {
+  return (
+    <div className="flex flex-col w-[300px] min-w-[300px] border-r min-h-screen p-4">
+      <div>
+        <UserItem />
+      </div>
+      <div className="grow">Menu</div>
+      <div>Settings/Notifications</div>
+    </div>
+  );
+}
---
+++ src/components/UserItem.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function UserItem() {
+  return (
+    <div>
+      <p>Axel Meister</p>
+      <p>meister.axel@gmail.com</p>
+    </div>
+  );
+}
